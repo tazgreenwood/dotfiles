77008bb3db05e79a4f98921929b9046be9eab0e9
tazlendio <taz.greenwood@lendio.com>
2022-01-28 11:16:02 -0700

    Cleaned up treesitter config file

--- nvim/lua/setup/treesitter.lua
+++ nvim/lua/setup/treesitter.lua
@@ -1,28 +1,24 @@
-require("nvim-treesitter.configs").setup({
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
-  autotag = {
-    enable = true,
-    filetypes = {
-      "html",
-      "javascript",
-      "typescript",
-      "markdown",
+require("nvim-treesitter.configs").setup {
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
     },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<CR>",
-      scope_incremental = "<CR>",
-      node_incremental = "<CR>",
-      node_decremental = "<TAB>",
+    rainbow = {
+        enable = true,
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
     },
-  },
-})
--- Setup treesitter
-local ts = require("nvim-treesitter.configs")
-ts.setup({ ensure_installed = "maintained", highlight = { enable = true } })
+    autotag = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<CR>",
+            node_decremental = "<BS>",
+        },
+    },
+}
+
