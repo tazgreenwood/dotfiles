2926caddb2853fd0a0b86bd2707cd7356038dc47
tazlendio <taz.greenwood@lendio.com>
2022-01-28 16:11:39 -0700

    updated monokai colorscheme colors and highlights

--- nvim/lua/setup/monokai.lua
+++ nvim/lua/setup/monokai.lua
@@ -2,27 +2,55 @@ require("monokai").setup{
     palette = {
         name = 'monokai_custom',
         base1 = '#211F22',
-        base2 = '#1f1f1f',
+        base2 = '#222222',
         base3 = '#2d2c2d',
         base4 = '#2d2c2d',
         base5 = '#4d5154',
         base6 = '#4d5154',
         base7 = '#B1B1B1',
-        border = '#bdbdbd',
+        border = '#fce566',
         brown = '#bdbdbd',
-        white = '#f7eeff',
+        white = '#f7f1ff',
         grey = '#69676c',
-        black = '#191919',
-        pink = '#ff518c',
-        green = '#53dc87',
-        aqua = '#00d7e9',
-        yellow = '#ffe648',
-        orange = '#ff8e42',
-        purple = '#9685e6',
-        red = '#ff518c',
-        diff_add = '#2C352E',
-        diff_remove = '#39292D',
-        diff_change = '#ff8e42',
-        diff_text = '#23324d',
+        black = '#222222',
+        pink = '#fc618d',
+        green = '#7bd88f',
+        aqua = '#5ad4e6',
+        yellow = '#fce566',
+        orange = '#fd9353',
+        purple = '#948ae3',
+        red = '#fc618d',
+    },
+    custom_hlgroups = {
+        GitSignsAdd = {
+            fg = '#7bd88f',
+            bg = '#222222'
+        },
+        GitSignsDelete = {
+            fg = '#fc618d',
+            bg = '#222222'
+        },
+        GitSignsChange = {
+            fg = '#fd9353',
+            bg = '#222222'
+        },
+        ColorColumn = {
+            bg = '#948ae3'
+        },
+        DiffAdd = {
+            fg = '#7bd88f',
+            bg = '#222222'
+        },
+        DiffChange = {
+            bg = '#2d2c2d'
+        },
+        DiffDelete = {
+            fg = '#fc618d',
+            bg = '#222222'
+        },
+        DiffText = {
+            fg = '#fd9353',
+            bg = '#2d2c2d'
+        }
     }
 }
