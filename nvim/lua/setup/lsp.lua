97329ac978bc834f5f2e580f212c7265b5e297f9
tazlendio <taz.greenwood@lendio.com>
2022-01-28 11:14:53 -0700

    Added diagnostic signs in the gutter

--- nvim/lua/setup/lsp.lua
+++ nvim/lua/setup/lsp.lua
@@ -45,3 +45,10 @@ for _, lsp in ipairs(servers) do
     }
   }
 end
+
+-- Setup diagnostic signs in the gutter
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
